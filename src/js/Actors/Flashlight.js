71bb8ee68a2eec5cb0d586d8e6379c3d0cf27b74
Sam-mid <sammiddendorp2002@gmail.com>
2023-06-16 15:52:45 +0200

    kleine verbeteringen

--- src/js/Actors/Flashlight.js
+++ src/js/Actors/Flashlight.js
@@ -1,41 +1,40 @@
-import { Actor, CollisionType, Graphic, Input, Scene, Sound, Vector } from "excalibur";
-import { Resources } from "../resources";
-import { Ghost } from "./Ghost";
+import {Actor, CollisionType, Graphic, Input, Scene, Sound, Vector} from "excalibur";
+import {Resources} from "../resources";
+import {Ghost} from "./Ghost";
 
-export class FlashLight extends Actor{
+export class FlashLight extends Actor {
 
-    DAMAGE =  5
+    DAMAGE = 5
 
     constructor() {
         super({
-            pos: new Vector(250, 0), 
+            pos: new Vector(250, 0),
             width: 220,
             height: Resources.Flashlight.height,
             CollisionType: CollisionType.Active,
-            
+
         })
     }
 
-    onInitialize(engine){
+    onInitialize(engine) {
         this.graphics.use(Resources.Flashlight.toSprite())
-        this.graphics.opacity =  0.4
+        this.graphics.opacity = 0.4
         this.on('collisionstart', (event) => {
             this.dealDamage(event)
         })
         console.log(`this is ` + this.DAMAGE)
     }
 
-    onPreUpdate(){
+    onPreUpdate() {
 
     }
 
-    dealDamage(event){
-        if (event.other instanceof Ghost){
+    dealDamage(event) {
+        if (event.other instanceof Ghost) {
             event.other.hitByFlashlight(this.DAMAGE)
 
         }
     }
-
 
 
 }
--- src/js/Actors/Ghost.js
+++ src/js/Actors/Ghost.js
@@ -4,46 +4,46 @@ import { Resources } from "../resources";
 export class Ghost extends Actor {
     HEALTH = 20;
     interval;
-  
+
     constructor() {
-      const circle = Shape.Circle(55);
-      super({
-        pos: new Vector(0, 0),
-        scale: new Vector(0.5, 0.5),
-        collider: circle,
-        collisionType: CollisionType.Active,
-      });
+        const circle = Shape.Circle(55);
+        super({
+            pos: new Vector(0, 0),
+            scale: new Vector(0.5, 0.5),
+            collider: circle,
+            collisionType: CollisionType.Active,
+        });
     }
-  
+
     onInitialize() {
-      this.graphics.use(Resources.Ghost.toSprite());
+        this.graphics.use(Resources.Ghost.toSprite());
     }
-  
+
     hitByFlashlight(DAMAGE) {
-      console.log(`The flashlight gives ${DAMAGE}`);
-      clearInterval(this.interval); // Clear any existing interval
-      this.interval = setInterval(() => {
-        this.updateHealthBar(DAMAGE);
-      }, 500);
+        console.log(`The flashlight gives ${DAMAGE}`);
+        clearInterval(this.interval); // Clear any existing interval
+        this.interval = setInterval(() => {
+            this.updateHealthBar(DAMAGE);
+        }, 500);
     }
-  
+
     updateHealthBar(DAMAGE) {
-      this.takeDamage(DAMAGE);
+        this.takeDamage(DAMAGE);
     }
-  
+
     takeDamage(DAMAGE) {
-      console.log(`Ghost: the amount taken is ${DAMAGE}`);
-      console.log(`Ghost: The current health is ${this.HEALTH}`);
-  
-      this.HEALTH -= DAMAGE;
-      console.log(this.HEALTH);
-  
-      this.HEALTH = Math.max(0, this.HEALTH);
-      this.HEALTH = Math.min(20, this.HEALTH);
-  
-      if (this.HEALTH === 0) {
-        this.kill();
-        clearInterval(this.interval);
-      }
+        console.log(`Ghost: the amount taken is ${DAMAGE}`);
+        console.log(`Ghost: The current health is ${this.HEALTH}`);
+
+        this.HEALTH -= DAMAGE;
+        console.log(this.HEALTH);
+
+        this.HEALTH = Math.max(0, this.HEALTH);
+        this.HEALTH = Math.min(20, this.HEALTH);
+
+        if (this.HEALTH === 0) {
+            this.kill();
+            clearInterval(this.interval);
+        }
     }
-  }
+}
