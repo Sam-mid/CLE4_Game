7f660a3be3a5834a9a6e07897f00de140e2a5335
JerrelTB <balkerjerrel@gmail.com>
2023-06-21 12:57:52 +0200

    Shortcut links en rechts

--- src/js/resources.js
+++ src/js/resources.js
@@ -1,16 +1,22 @@
 import { ImageSource, Sound, Resource, Loader, SpriteSheet, Animation, range } from 'excalibur'
 import fishImage from '../images/fish.png'
-import player from '../images/GuardSprite.png'
-import flashlight from '../images/flashlight.png'
+//etc
+import logo from '../images/Logo.png'
+import StartButton from '../images/StartButton.png'
+
+//Ghost
 import ghost from '../images/ghostsketch.png'
+
+//Map and objects
 import map from '../images/Map/placeholdermap.png'
 import bench from '../images/bench.png'
 import plant from '../images/plant.png'
-import logo from '../images/Logo.png'
-import StartButton from '../images/StartButton.png'
 import background from '../images/background.png';
 import train from '../images/train.png';
 
+//Player
+import player from '../images/GuardSprite.png'
+import flashlight from '../images/flashlight.png'
 
 const Resources = {
     Fish: new ImageSource(fishImage),
